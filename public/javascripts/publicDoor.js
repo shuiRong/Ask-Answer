071c93b38ac9d2a06ada9adf5e72241c6d19c735
shuirong <shuirong1997@icloud.com>
2017-03-05 19:00:13 +0800

    完成登录注册页的样式信息

--- public/javascripts/publicDoor.js
+++ public/javascripts/publicDoor.js
@@ -1,7 +1,19 @@
 $(document).ready(function(){
-    listen();
+    publicDoorListen();
 });
 
-function listen(){
-    $('')
+//登录页的一些监听
+function publicDoorListen(){
+    $('#signInBtn').click(function(){
+        $('#signUpBtn').css('color','grey')    ;
+        $('#signInBtn').css('color','black')    ;
+        $('#signInForm').css('display','flex');
+        $('#signUpForm').css('display','none');
+    });
+    $('#signUpBtn').click(function(){
+        $('#signUpBtn').css('color','black')    ;
+        $('#signInBtn').css('color','grey');
+        $('#signInForm').css('display','none');
+        $('#signUpForm').css('display','flex');
+    });
 }
